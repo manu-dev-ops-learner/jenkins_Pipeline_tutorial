43486692a6c1a16f1eccbe6507744688be77cf76
manuel <manuel.kouakou@inphb.ci>
2021-06-15 16:11:56 +0200

    Case when user number is greater than 9

--- plus_ou_moins.py
+++ plus_ou_moins.py
@@ -9,9 +9,20 @@ def plus_ou_moins():
     user_number = int(input("Entrez un chiffre entre 0 et 9 : "))
     random_number = random.randint(0,9)
 
-    #Compare
+    #If user number > 9
+
+
+    #Compare user given number with number choices by computer
     while (user_number != random_number):
-        user_number = int(input("Une autre chance, entrez un chiffre entre 0 et 9 : "))
+
+        #When user number > 9
+        if (user_number > 9):    
+            print("Oups, entre 0 et 9 on a dit !")
+            user_number = int(input("Allez une autre chance, entrez un chiffre entre 0 et 9 : "))
+        
+        else :    
+            user_number = int(input("Pas de bol, retentez un chiffre entre 0 et 9 : "))
+    
 
     if (user_number==random_number) :
         print ('Bravo vous avez vu juste !', "\N{winking face}")
@@ -19,6 +30,7 @@ def plus_ou_moins():
         #print("Nul, l'ordi a choisi", random_number)
     
 #print(sys.version)
+#Add execption on float and string
 
 #Function call
 #plus_ou_moins()
